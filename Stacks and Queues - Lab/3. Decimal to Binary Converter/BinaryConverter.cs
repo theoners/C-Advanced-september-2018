22f4f547dd622dbe7a805714b019ced66c6aca98
theoners <theonebgrs@gmail.com>
2018-12-21 17:17:06 +0200

    added Stacks and Queues - Lab

---
+++ Stacks and Queues - Lab/3. Decimal to Binary Converter/BinaryConverter.cs
@@ -0,0 +1,34 @@
+﻿namespace _3._Decimal_to_Binary_Converter
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BinaryConverter
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var binaryNumber = new Stack<int>();
+
+            if (number == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            while (number != 0)
+            {
+                int result = number % 2;
+                binaryNumber.Push(result);
+                number /= 2;
+
+            }
+
+            while (binaryNumber.Count != 0)
+            {
+                Console.Write(binaryNumber.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/6. Traffic Jam/Traffic.cs
@@ -0,0 +1,43 @@
+﻿namespace _6._Traffic_Jam
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Traffic
+    {
+        public static void Main()
+        {
+            var numberOfCars = int.Parse(Console.ReadLine());
+            var totalCarsInTraffic = new Queue<string>();
+            var carCounter = 0;
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input=="end")
+                {
+                    break;
+                }
+                if (input == "green")
+                {
+                    for (int i = 0; i < numberOfCars; i++)
+                    {
+                        if (totalCarsInTraffic.Count != 0)
+                        {
+                            Console.WriteLine(totalCarsInTraffic.Dequeue()+" passed!");
+                            carCounter++;
+                        }
+
+                    }
+                }
+                else
+                {
+                    totalCarsInTraffic.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{carCounter} cars passed the crossroads.");
+
+        }
+    }
+}
