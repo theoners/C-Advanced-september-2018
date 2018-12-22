0e12d3869f52ea441b53f5d9cf16da51b9f63633
theoners <theonebgrs@gmail.com>
2018-12-22 21:26:28 +0200

    added Multidimensional Arrays - Lab

---
+++ Multidimensional Arrays - Lab/7. Group Numbers/GroupNumbers.cs
@@ -0,0 +1,38 @@
+﻿namespace _7._Group_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GroupNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            List<int> remainderZero = new List<int>();
+            List<int> remainderOne = new List<int>();
+            List<int> remainderTwo = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                if (numbers[i]%3==0)
+                {
+                    remainderZero.Add(numbers[i]);
+                }
+                else if (Math.Abs(numbers[i])%3==1)
+                {
+                    remainderOne.Add(numbers[i]);
+                }
+                else
+                {
+                    remainderTwo.Add(numbers[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", remainderZero));
+            Console.WriteLine(string.Join(" ", remainderOne));
+            Console.WriteLine(string.Join(" ", remainderTwo));
+        }
+    }
+}
