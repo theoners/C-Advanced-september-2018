bdc8222a1037eda34e739a1f33340ddb8cf81c41
theoners <theonebgrs@gmail.com>
2018-12-27 00:44:34 +0200

    added Sets and Dictionaries Advanced - Exercicse

---
+++ Sets and Dictionaries Advanced - Exercicse/05. Count Symbols/CountSymbol.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CountSymbol
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var charCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!charCount.ContainsKey(input[i]))
+                {
+                    charCount.Add(input[i],0);
+                }
+
+                charCount[input[i]]++;
+            }
+
+            foreach (var symbol in charCount.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
