331059974e9a3c942ffc70e275040155ef9c31d1
theoners <theonebgrs@gmail.com>
2019-02-03 20:08:03 +0100

    added Functional Programming

---
+++ Functional Programming - Exercises/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _05._Applied_Arithmetics
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var listOfNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command=="end")
+                {
+                    break;
+                }
+
+                Func<int, int> manipulateList;
+
+                switch (command)
+                {
+                    case "add":
+                        manipulateList = x => x + 1;
+                        break;
+                    case "subtract":
+                        manipulateList = x => x - 1;
+                        break;
+                    case "multiply":
+                        manipulateList = x => x * 2;
+                        break;
+                    default:
+                        manipulateList = x => x;
+                        break;
+                }
+
+              listOfNumbers= listOfNumbers.Select(manipulateList).ToList();
+
+                if (command=="print")
+                {
+                    Console.WriteLine(string.Join(" ", listOfNumbers));
+                }
+            }
+        }
+    }
+}
---
+++ Functional Programming - Exercises/08. Custom Comparator/CustomComparator.cs
@@ -0,0 +1,19 @@
+﻿namespace _08._Custom_Comparator
+{
+    using System;
+    using System.Linq;
+
+    public class CustomComparator
+    {
+        public static void Main()
+        {
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderBy(x => x % 2 != 0)
+                .ThenBy(x => x)
+                .ToList()
+                .ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
